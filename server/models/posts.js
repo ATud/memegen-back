b8a34bbcaec4e367c74d7fb640ddcb62f051cca7
musatandrei <musatandrei22@yahoo.com>
2017-10-12 14:46:06 +0300

    First commit

---
+++ server/models/posts.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var Posts = sequelize.define('Posts', {
+        url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        length:500
+    },
+        {
+          classMethods: {
+            associate: function(models) {
+              // associations can be defined here
+            }
+          }
+        });
+      return Posts;
+    };
---
+++ server/models/users.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Users = sequelize.define('Users', {
+  username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        length:256
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        length:256
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        length:500
+    },
+    avatarUrl: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        length:500
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+    
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Users;
+};
